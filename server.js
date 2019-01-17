b8432318ff93e2b1c93a68e0e95654757485f193
Guillaume Bedard <bgui34@gmail.com>
2019-01-16 20:18:48 -0500

    Version 1.0.0

--- server.js
+++ server.js
@@ -1,25 +1,36 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const items = require("./routes/api/items")
+const express = require('express');
+const mongoose = require('mongoose');
+const items = require('./routes/api/items');
+const path = require('path');
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 //Bodyparser was remplaced by express.json
 
 //Db Config
-const db = require("./config/keys").mongoURI;
+const db = require('./config/keys').mongoURI;
 
 //Connect to mongo
 mongoose
-    .connect(db, {
-        useNewUrlParser: true
-    })
-    .then(() => console.log("MongoDB connected"))
-    .catch(err => console.log(err));
+	.connect(db, {
+		useNewUrlParser: true
+	})
+	.then(() => console.log('MongoDB connected'))
+	.catch((err) => console.log(err));
 
 //Use Routes
-app.use('/api/items', items)
+app.use('/api/items', items);
+
+//Serve statuc assest if in production
+if (process.env.NODE_ENV == 'production') {
+	//Set static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 const port = process.env.PORT || 5000;
 
