f31009cd9c60fe92af14aa5cce9f34202c485f09
Guillaume Bedard <bgui34@gmail.com>
2019-01-17 01:11:08 -0500

    Fixed CreateStore

--- client/src/store.js
+++ client/src/store.js
@@ -1,25 +1,14 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
-import { composeWithDevTools } from 'redux-devtools-extension';
+/* import { composeWithDevTools } from 'redux-devtools-extension'; */
 
 const initialState = {};
 
 const middleware = [ thunk ];
 
-const composeEnhancers = composeWithDevTools(
-	{
-		// options like actionSanitizer, stateSanitizer
-	}
-);
-const store = createStore(
-	rootReducer,
-	initialState,
-	composeEnhancers(
-		applyMiddleware(...middleware)
-		// other store enhancers if any
-	)
-);
+const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
+const store = createStore(rootReducer, initialState, composeEnhancers(applyMiddleware(...middleware)));
 
 /* const store = createStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(...middleware))); */
 
